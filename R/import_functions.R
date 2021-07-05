560af6b3a8b31558e0ec0284f4a9b94ffe784240
Matthew Harris <m.r.p.harris@keele.ac.uk>
2021-07-05 12:27:28 +1000

    PEM import function added

---
+++ R/import_functions.R
@@ -0,0 +1,42 @@
+# Import functions for use with fluorometer data.
+
+#' An import function for use with processed excitation-emission matrix (PEM) ASCII files from the HORIBA Aqualog. Essentially identical to eemR::eem_import_aqualog, save for a single line.
+#'
+#' @description To be used within eemR::eem_read() to import aqualog PEM ASCII data files.
+#'
+#' @param file path to file
+#'
+#' @export
+#'
+eem_read_aqualog_PEM <- function(file){
+  # Initial data read
+  file = dat_files[1]
+  data <- readLines(file)
+  eem <- stringr::str_extract_all(data, "-?\\d+(?:\\.\\d*)?(?:[eE][+\\-]?\\d+)?")
+  ex <- sort(as.numeric(eem[[1]]))
+
+  # data manip
+  n_col <- lapply(eem, length)
+  n_col <- unlist(n_col)
+  expected_col <- as.numeric(names(sort(-table(n_col)))[1])
+
+  eem[n_col != expected_col] <- NULL
+  eem <- lapply(eem, as.numeric)
+  eem <- do.call(rbind, eem)
+
+
+  em <- eem[, 1] # extracting emission axis.
+  eem <- eem[, -1] # removing emission axis
+  # The line below replaces the hashed line after it.
+  eem <- as.matrix(eem) # new as.matrix() without reversal.
+  # Original as.matrix() call from eem_import_aqualog(). Led to incorrect reversal of the eem data along the excitation axis.
+  #eem2 <- as.matrix(eem[, ncol(eem):1])
+
+  l <- list(
+    file = file,
+    x = eem,
+    em = em,
+    ex = ex # change made here.
+  )
+  return(l)
+}
