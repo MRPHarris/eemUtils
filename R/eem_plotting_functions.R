145f917eaacddfd2b1db898c5097fd6619618d9d
Matthew Harris <m.r.p.harris@keele.ac.uk>
2021-09-30 15:16:35 +1000

    ggeem2 legend text tweak

--- R/eem_plotting_functions.R
+++ R/eem_plotting_functions.R
@@ -285,7 +285,7 @@ ggeem2 <- function(eem,
       strip.text = element_blank(),
       axis.text = element_text(size = 9*textsize_multiplier),
       axis.title = element_text(size = 10*textsize_multiplier),
-      legend.text = element_test(size = 9*textsize_multiplier)
+      legend.text = element_text(size = 9*textsize_multiplier)
     )
   # Border rectangle
   plot  <- plot +
