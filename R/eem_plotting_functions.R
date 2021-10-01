f3c249f8c7ba0b6fadac8c79baf59e67e36ac49f
Matthew Harris <m.r.p.harris@keele.ac.uk>
2021-10-01 17:20:46 +1000

    multiple plot handling for ggeem2

--- R/eem_plotting_functions.R
+++ R/eem_plotting_functions.R
@@ -186,15 +186,82 @@ ggeem2.parafac <- function(data, fill_max = FALSE, ...)
 #' @rdname ggeem2
 #' @export
 ggeem2.data.frame <- function(data,
-                                 fill_max = FALSE,
-                                 title_text = NULL,
-                                 bin_vals = NULL,
-                                 colpal = "12pal",
-                                 contour = TRUE,
-                                 interpolate = FALSE,
-                                 redneg = NULL,
-                                 legend = TRUE,
-                                 textsize_multiplier = 1,...){
+                              n_eems = 1,
+                              fill_max = FALSE,
+                              title_text = NULL,
+                              bin_vals = NULL,
+                              colpal = "12pal",
+                              contour = TRUE,
+                              interpolate = FALSE,
+                              redneg = NULL,
+                              legend = TRUE,
+                              textsize_multiplier = 1,...){
+  # Colpal handling
+  if(colpal[1] == "12pal"){
+    #colpal <- (function(...)get(data(...,envir = new.env())))(eem_palette_12) # thanks henfiber https://stackoverflow.com/questions/30951204/load-dataset-from-r-package-using-data-assign-it-directly-to-a-variable
+    data("eem_palette_12")
+    colpal <- eem_palette_12
+    message("Using default colour palette")
+    if(sum(eem$x < 0, na.rm = TRUE) == 0){
+      newpal <- colorRampPalette(c(colpal[2:length(colpal)]))
+      colpal <- newpal(12)
+    }
+  } else if(colpal[1] == "rainbow"){
+    colpal <- rainbow(75)[53:1]
+    message("Using rainbow colour palette")
+  } else if(!is.vector(colpal)) {
+    stop("Please provide a vector of colours, or use the defaults!")
+  }
+  if(n_eems == 0){
+    stop("n_eems set to 0. Please provide a number of eems")
+  } else if(n_eems == 1){
+    # one EEM.
+    plot <- ggeem2_single(data,
+                          n_eems = n_eems,
+                          fill_max = fill_max,
+                          title_text = title_text,
+                          bin_vals = bin_vals,
+                          colpal = colpal,
+                          contour = contour,
+                          interpolate = interpolate,
+                          redneg = redneg,
+                          legend = legend,
+                          textsize_multiplier = textsize_multiplier,...)
+    plot
+  } else {
+    #data <- table
+    plot <- ggeem2_multi(data,
+                         n_eems = n_eems,
+                         fill_max = fill_max,
+                         title_text = title_text,
+                         bin_vals = bin_vals,
+                         colpal = colpal,
+                         contour = contour,
+                         interpolate = interpolate,
+                         redneg = redneg,
+                         legend = legend,
+                         textsize_multiplier = textsize_multiplier,...)
+    plot
+  }
+}
+
+#' Single plot handler for ggeem2.
+#'
+#' @description Plot a single EEM for ggeem2. Params inherit from ggeem2.
+#'
+#' @noRd
+#'
+ggeem2_single <- function(data,
+                          n_eems = n_eems,
+                          fill_max = fill_max,
+                          title_text = title_text,
+                          bin_vals = bin_vals,
+                          colpal = colpal,
+                          contour = contour,
+                          interpolate = interpolate,
+                          redneg = redneg,
+                          legend = legend,
+                          textsize_multiplier = textsize_multiplier,...){
   eem <- data
   eem_constructed <- eemdf_to_eem(eemdf = eem,
                                   file = NULL,
@@ -377,5 +444,107 @@ ggeem2.data.frame <- function(data,
   #  )
   #}
   plot
+
 }
 
+#' Multi-plot handler for ggeem2.
+#'
+#' @description Plot a single EEM for ggeem2. Params inherit from ggeem2.
+#'
+#' @noRd
+#'
+ggeem2_multi <- function(data,
+                         n_eems = n_eems,
+                         fill_max = fill_max,
+                         title_text = title_text,
+                         bin_vals = bin_vals,
+                         colpal = colpal,
+                         contour = contour,
+                         interpolate = interpolate,
+                         redneg = redneg,
+                         legend = legend,
+                         textsize_multiplier = textsize_multiplier,...){
+  if(!is.null(bin_vals)){
+    message("Binning not supported for multiple EEMs with ggeem2. Skipping binning.")
+  }
+  if(!is.null(redneg)){
+    warning("redneg is deprecated and will be ignored! Please use the argument 'colpal = c(rainbow(75)[58],rainbow(75)[51:1])' to produce similar behaviour.")
+  }
+  table <- data %>%
+    mutate_at(vars(ex,em,value),as.numeric)
+  if(!is.numeric(fill_max)){
+    fill_max <- table$value %>% max(na.rm=TRUE)
+  }
+  diffs <- table %>%
+    select(-value) %>%
+    gather("spec","wl", -sample) %>%
+    group_by(sample,spec) %>%
+    unique() %>%
+    summarise(slits = diff(wl) %>% n_distinct()) %>%
+    .$slits != 1
+
+  plot <- table %>%
+    ggplot(aes(x = ex, y = em, z = value))+
+    labs(x = "Excitation (nm)", y = "Emission (nm)")
+
+  if(any(diffs)){
+    plot <- plot +
+      layer(mapping = aes(colour = value, fill = value),
+            geom = "tile", stat = "identity", position = "identity")
+  } else {
+    plot <- plot +
+      layer(mapping = aes(fill = value),
+            geom = "raster", stat = "identity", position = "identity")
+  }
+
+  plot <- plot +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+    facet_wrap(~ sample)
+  if(contour){
+    plot <- plot +
+      geom_contour(colour = "black", size = 0.3)
+  }
+  if(table$value %>% min(na.rm=TRUE) < 0){
+    vals <- c(table$value %>% min(na.rm = TRUE), seq(from = 0, to = fill_max, length.out = length(colpal) - 1))
+    vals <- (vals - min(vals))/diff(range(vals))
+    plot <- plot +
+      scale_fill_gradientn(colours = colpal, values = vals, limits = c(table$value %>% min(na.rm=TRUE),fill_max),
+                           na.value="white")+
+      scale_colour_gradientn(colours = colpal, values = vals, limits = c(table$value %>% min(na.rm=TRUE),fill_max),
+                             na.value="white") +
+      scale_x_continuous(expand = c(0,0)) +
+      scale_y_continuous(expand = c(0,0))
+  } else {
+    plot <- plot +
+      scale_fill_gradientn(colours = colpal, limits = c(0,fill_max), na.value="white")+
+      scale_colour_gradientn(colours = colpal, limits = c(0,fill_max), na.value="white") +
+      scale_x_continuous(expand = c(0,0)) +
+      scale_y_continuous(expand = c(0,0))
+  }
+  if(!is.null(title_text)){
+    plot <- plot +
+      labs(title = title_text) +
+      theme(plot.title = element_text(hjust = 0.5, size = 11*textsize_multiplier, face = "bold"))
+  } else {
+    plot <- plot +
+      theme(plot.title = element_blank())
+  }
+  plot <- plot +
+    theme(
+      panel.background = element_rect(fill = 'white', colour = 'black'),
+      strip.background = element_blank(),
+      strip.text = element_blank(),
+      axis.text = element_text(size = 9*textsize_multiplier),
+      axis.title = element_text(size = 10*textsize_multiplier),
+      legend.text = element_text(size = 9*textsize_multiplier),
+      legend.title = element_text(size = 10*textsize_multiplier)
+    )
+  #plot  <- plot +
+  #  annotate(geom = "rect", xmin = min(eem_constructed$ex)-(x_slit_min/2), xmax = max(eem_constructed$ex)+(x_slit_max/2), ymin = min(eem_constructed$em)-(y_slit_min/2), ymax = max(eem_constructed$em)+(y_slit_max/2),
+  #           colour = "black", fill = "white", alpha = 0, size = 0.5)
+  if(!isTRUE(legend)){
+    plot <- plot +
+      theme(legend.position = "none")
+  }
+  plot
+}
