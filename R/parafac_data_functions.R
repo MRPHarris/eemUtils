52ad7648a537350d7c412a641c38c5211c653d5e
Matthew Harris <m.r.p.harris@keele.ac.uk>
2022-10-27 15:38:50 +1100

    updated PARAFAC comp peak positions function added

--- R/parafac_data_functions.R
+++ R/parafac_data_functions.R
@@ -130,37 +130,23 @@ extrpf_eems <- function(pfmodel){
   eemlist
 }
 
-#' Determines the peak positions for a PARAFAC modeled component.
+#' Return the ex/em position of the peak maxima for a PARAFAC modeled component.
 #'
-#' @description DEFUNCT Determine the Ex/Em peak position of a PARAFAC component. Takes
-#'      an output from extrpf_spectra_or_eems(type = 2).
+#' @description Uses extrpf_peak_spectra to obtain the single ex/em coordinate pair for the maxima of the
+#'     specified PARAFAC component.
 #'
 #' @param modeled_spectra PARAFAC-modeled spectra. An output from extrpf_spectra_or_eems(type = 2)
 #'
 #' @export
 #'
-peak_positions <- function(modeled_spectra){
-  peak_list <- vector(mode = "list", length = length(modeled_spectra))
-  for(i in seq_along(modeled_spectra)){
-    spectra <- modeled_spectra[[i]]
-    ncomponents <- as.numeric(unique(spectra[["comps"]]))
-    peak_coords <- data.frame(matrix(NA,ncomponents,2))
-    maxem <- rle(spectra[["max_em"]])[[2]]
-    maxex <- rle(spectra[["max_ex"]])[[2]]
-    peak_coords[1:length(maxex),1] = maxex
-    peak_coords[1:length(maxem),2] = maxem
-    rownames_vals <- seq(1:(ncomponents))
-    rownames(peak_coords) = paste("Component_",rownames_vals,sep = "")
-    col1 = paste0("Peak Excitation")
-    col2 = paste0("Peak Emission")
-    colnames(peak_coords) = c(col1, col2)
-    # add to list instead
-    peak_list[[i]] <- peak_coords
-  }
-  print(peak_list)
-  peak_list <- peak_list
+extrpf_peak_positions <- function(pfmodel, component = 1){
+  peak_spectra = extrpf_peak_spectra(pfmodel = pfmodel,
+                                     component = component)
+  peakpositions = peak_spectra %>%
+    dplyr::select(max_ex,max_em) %>%
+    distinct()
+  peakpositions
 }
-
 
 #' Extract spectra at the peak wavelength position.
 #'
